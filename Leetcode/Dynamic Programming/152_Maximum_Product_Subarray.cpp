5297c8c6762af3d73e96601cfc2e31e7c79fee50
KKapoor0801 <58838197+KKapoor0801@users.noreply.github.com>
2022-06-14 20:39:29 +0530

    Completed Leetcode 152. Approach is new and nice!

---
+++ Leetcode/Dynamic Programming/152_Maximum_Product_Subarray.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+        int overall_max = nums[0];
+        int n = nums.size();
+        
+        vector<int>dp_max_ending_here(n); // if nums[i] is +ve then it can make it even bigger if max is +ve
+        vector<int>dp_min_ending_here(n); // if nums[i] is -ve then it can make it even bigger if min is -ve
+        
+        dp_max_ending_here[0] = nums[0];  
+        dp_min_ending_here[0] = nums[0];
+        
+        // [2,-3,-4,-4,5] --> use this eg for understanding the use of 2 dp arrays for max and min.
+        
+        for(int i = 1 ; i<n ; i++){
+            
+            dp_max_ending_here[i] = max(dp_max_ending_here[i-1] * nums[i], 
+                                        max(dp_min_ending_here[i-1] * nums[i], nums[i]));
+            
+            dp_min_ending_here[i] = min(dp_min_ending_here[i-1] * nums[i], 
+                                        min(dp_max_ending_here[i-1] * nums[i], nums[i]));
+            
+            overall_max = max(overall_max, dp_max_ending_here[i]);
+        }
+        
+        return overall_max;
+        
+    }
+};
