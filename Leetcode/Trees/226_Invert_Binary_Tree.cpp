266df3da13298789b102a4e4f35077d58e5f8c16
KKapoor0801 <58838197+KKapoor0801@users.noreply.github.com>
2022-06-22 22:37:32 +0530

    Completed Leetcode 226. Need to learn syntax.

---
+++ Leetcode/Trees/226_Invert_Binary_Tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    
+    void swap(TreeNode* root){
+        
+        if(root == NULL)
+            return;
+        
+        swap(root->left);
+        swap(root->right);
+        
+        TreeNode* temp = root->right;
+        root->right = root->left;
+        root->left = temp;
+        
+    }
+    
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        swap(root);
+        return root;
+    }
+};
