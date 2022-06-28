9775c54bf9239066c404793128d32274e61a9e72
KKapoor0801 <58838197+KKapoor0801@users.noreply.github.com>
2022-06-29 02:36:02 +0530

    Update 200_Number_of_Islands.cpp

--- Leetcode/Graph/200_Number_of_Islands.cpp
+++ Leetcode/Graph/200_Number_of_Islands.cpp
@@ -3,14 +3,15 @@ public:
     
     void countIslands(vector<vector<char>>& grid, int i, int j, int m, int n){
         
-        if(i<0 || j<0 || i>=m || j>=n)
+        if(i<0 || j<0 || i>=m || j>=n)  // Even the surrounding boundaries are considered as water
             return;
         
         if(grid[i][j] == '0')
             return;
         
-        grid[i][j] = '0';
+        grid[i][j] = '0';  // Marking the land as '0' after visiting it
         
+        // Trying all possible combinations in all 4 directions to find a land 
         countIslands(grid,i+1,j,m,n);
         countIslands(grid,i-1,j,m,n);
         countIslands(grid,i,j+1,m,n);
